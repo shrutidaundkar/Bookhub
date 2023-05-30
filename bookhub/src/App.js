849571a1be194540660f1400497105cb80c69f76
mahathi0412 <mandelli@pdx.edu>
2023-05-30 12:44:58 -0700

    Created list of options on the home page

--- bookhub/src/App.js
+++ bookhub/src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      BookHub
+      <div className="sidebar">
+        <div className="logo"><h3>BookHub</h3></div>
+        <ul className="options">
+          <li>Home</li>
+          <li>Genres</li>
+          <li>My Books</li>
+          <li>Trending</li>
+          <li>Favourites</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div className="content">
+        {/* Content of your dashboard goes here */}
+      </div>
     </div>
   );
 }
